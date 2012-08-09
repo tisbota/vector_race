36698a62a650e019c528aac7df8b0f370b0fcf28
Johan Frølich <johanfrolich@gmail.com>
2012-08-09 14:55:39 +0200

    update gitignore and car model

--- src/model/Car.java
+++ src/model/Car.java
@@ -5,19 +5,32 @@ import java.awt.Point;
 public class Car {
 	
 	//Fields
-	public Point position = new Point();
+	private Point position = new Point();
 	private Point velocity = new Point(0,0);
-	public Color color;
+	private Color color;
 	
-	/**
-	 * 
-	 * @param color
-	 * @param position
-	 * 
-	 */
 	public Car(Color color, Point position){
 		this.color = color;
 		this.position = position;
+	}
+	
+	public Point getPosition(){
+		return position;
+	}
+	
+	public Color getColor(){
+		return color;
+	}
+	
+	public void moveCar(Point speed){
+		if (Board.pathOnBoard(position, speed) ){
+			velocity.translate(speed.x, speed.y);
+			position.translate(velocity.x, velocity.y);
+		} else {
+			velocity.setLocation(0, 0);
+			position = Board.putBackOnBoard(position, speed);
+		}
+		
 	}
 	
 
